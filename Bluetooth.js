d8a0a6e519e8b4422767b9ed1716f9108de003fe
bernalcristiancamilo93 <65104057+bernalcristiancamilo93@users.noreply.github.com>
2021-04-15 23:53:05 -0500

    Add files via upload

---
+++ Bluetooth.js
@@ -0,0 +1,39 @@
+function connectToBle() {
+  // Connect to a device by passing the service UUID
+  bluetooth.connect(0xFFE0, gotCharacteristics);
+}
+
+function disconnectToBle() {
+  // Disonnect to the device
+  bluetooth.disconnect();
+  // Check if myBLE is connected
+  isConnected = bluetooth.isConnected();
+}
+
+function onDisconnected() {
+  console.log('Device got disconnected');
+  isConnected = false;
+}
+
+// A function that will be called once got characteristics
+function gotCharacteristics(error, characteristics) {
+  if (error) {
+    console.log('Error: ', error);
+  }
+  console.log('Characteristics: ', characteristics);
+
+  // Check if myBLE is connected
+  isConnected = bluetooth.isConnected();
+
+  // Add a event handler when the device is disconnected
+  bluetooth.onDisconnected(onDisconnected)
+}
+
+function sendData(command) {
+  const inputValue = command;
+  if (!("TextEncoder" in window)) {
+    console.log("Sorry, this browser does not support TextEncoder...");
+  }
+  var enc = new TextEncoder(); // always utf-8
+  blueToothCharacteristic.writeValue(enc.encode(inputValue));
+}
