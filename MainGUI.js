018741fb14246e5beff9ce71c471bd39a63d70c2
bernalcristiancamilo93 <65104057+bernalcristiancamilo93@users.noreply.github.com>
2021-04-16 23:36:51 -0500

    Update MainGUI.js

--- MainGUI.js
+++ MainGUI.js
@@ -1,28 +1,25 @@
-// let blueToothCcteristic;
-// let receivedValue = "";
-
-let bluetooth;
-let isConnected = false;
-
-
-// var millisecondTimerStart;
-// var oldColorPickerValue;
-
-
 function setup() {
+  
+  voiceSettings();
 
   createCanvas(windowWidth, windowHeight);
+
+  rectMode(CENTER);
+
+
+
+  colorCuadrado = color(0, 0, 0);
 
   // Create a p5ble class
   console.log("Waiting for connection WebBluetooth...");
   bluetooth = new p5ble();
 
   // Create a 'Connect' button
-  const connectButton = createButton('Conectar')
+  const connectButton = createButton('Conectar');
   connectButton.mousePressed(connectToBle);
 
   // Create a 'Disconnect' button
-  const disconnectButton = createButton('Desconectar')
+  const disconnectButton = createButton('Desconectar');
   disconnectButton.mousePressed(disconnectToBle);
 
   // Button pos
@@ -32,7 +29,6 @@ function setup() {
   connectButton.position(posX, posY);
   disconnectButton.position(posX + connectButton.width, posY);
 }
-
 
 function draw() {
   drawScreen();
